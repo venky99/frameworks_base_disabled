cc0da580d30214a8110fc436d11400481d6812d9
sbrissen <sbrissen@Hotmail.com>
2012-05-25 09:27:31 -0400

    Fix for Signal bars on Samsung devices
    Sometimes the CDMA ecio value is sent as a negative integer where we would expect a positive.
    This causes SignalStrength.java to mark ecio as -160 (Unknown_Signal_Strength) and making signal bar
    s clear out.
    This "fix" checks the ecio value and takes it absolute value if it is negative.
    
    Confirmed working on SCH-I500 and SPH-D710
    
    Change-Id: Ic515dd6c92193cd5db75adf5bef93ecd617a5c6a
    
    Conflicts:
    	telephony/java/com/android/internal/telephony/SamsungRIL.java

--- telephony/java/com/android/internal/telephony/SamsungRIL.java
+++ telephony/java/com/android/internal/telephony/SamsungRIL.java
@@ -854,11 +854,14 @@ public class SamsungRIL extends RIL implements CommandsInterface {
         for (int i = 7; i < numInts; i++) {
             response[i] = -1;
         }
-		
-		if (mIsSamsungCdma)
+
+        if (mIsSamsungCdma){
+            if(response[3] < 0){
+               response[3] = -response[3];
+            }
             // Framework takes care of the rest for us.
             return response;
-
+         }
         /* Matching Samsung signal strength to asu.
 		   Method taken from Samsungs cdma/gsmSignalStateTracker */
         if(mSignalbarCount)
