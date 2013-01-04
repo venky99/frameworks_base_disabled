0798a4b6ea47ee688a1b61b15b5089e30914a263
Alin Jerpelea <jerpelea@gmail.com>
2013-01-04 21:24:18 +0200

    ST_ERICSSON: Adding multimedia extension part2
    manual import from
    commit 151ce20c17e1a097a2edf5b467650941a1cc6287
    Author: Patrik Ryd <patrik.ryd@stericsson.com>
    Date:   Thu Apr 5 20:18:30 2012 +0000
    
        Adding multimedia extension
    
        More specifically:
        - ST-Ericsson audio support for multimedia.
        - Support for FM Audio in framework.
    
    Change-Id: Iea956cb398793023cf8e8c003089ae6aef7fb843

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -43,6 +43,9 @@
 #endif
 #include <media/stagefright/DataSource.h>
 #include <media/stagefright/FileSource.h>
+#ifdef STE_FM
+#include <media/stagefright/FMRadioDataSource.h>
+#endif
 #include <media/stagefright/MediaBuffer.h>
 #include <media/stagefright/MediaDefs.h>
 #include <media/stagefright/MediaExtractor.h>
@@ -2132,6 +2135,9 @@ status_t AwesomePlayer::prepareAsync_l() {
 status_t AwesomePlayer::finishSetDataSource_l() {
     sp<DataSource> dataSource;
 
+#ifdef STE_FM
+    char *mime = NULL;
+#endif
     bool isWidevineStreaming = false;
     if (!strncasecmp("widevine://", mUri.string(), 11)) {
         isWidevineStreaming = true;
@@ -2270,6 +2276,17 @@ status_t AwesomePlayer::finishSetDataSource_l() {
                 return UNKNOWN_ERROR;
             }
         }
+#ifdef STE_FM
+    } else if (!strncasecmp("fmradio://rx", mUri.string(), 12)) {
+
+        mime = (char*) MEDIA_MIMETYPE_AUDIO_RAW;
+        // HACK: Removed the line below since it causes problems. Add it back if you want FM-Radio.
+        dataSource = new FMRadioDataSource();
+        status_t err = dataSource->initCheck();
+        if (err != OK) {
+            return err;
+        }
+#endif
     } else {
         dataSource = DataSource::CreateFromURI(mUri.string(), &mUriHeaders);
     }
@@ -2296,9 +2313,12 @@ status_t AwesomePlayer::finishSetDataSource_l() {
         mWVMExtractor->setAdaptiveStreamingMode(true);
         extractor = mWVMExtractor;
     } else {
+#ifdef STE_FM
+        extractor = MediaExtractor::Create(dataSource, mime);
+#else
         extractor = MediaExtractor::Create(
                 dataSource, sniffedMIME.empty() ? NULL : sniffedMIME.c_str());
-
+#endif
         if (extractor == NULL) {
             return UNKNOWN_ERROR;
         }
