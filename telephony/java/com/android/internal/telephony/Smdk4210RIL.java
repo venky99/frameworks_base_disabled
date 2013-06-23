CyanogenMod/android_frameworks_base

Smdk4210RIL: swap voice privacy and video
Change-Id: Idadf8e41155fbb9b5baf2cad8cbb1199507517a9

telephony/java/com/android/internal/telephony/Smdk4210RIL.java
@@ -631,6 +631,7 @@ public class Smdk4210RIL extends RIL implements CommandsInterface {
     responseCallList(Parcel p) {
         int num;
         int voiceSettings;
+        int videoSettings;
         ArrayList<DriverCall> response;
         DriverCall dc;
         int dataAvail = p.dataAvail();
@@ -666,11 +667,10 @@ public class Smdk4210RIL extends RIL implements CommandsInterface {
             voiceSettings = p.readInt();
             dc.isVoice = (0 == voiceSettings) ? false : true;
             Log.d(LOG_TAG, "isVoice = " + dc.isVoice);
+            // For samsungs videocall feature
+            videoSettings = p.readInt();
+            Log.d(LOG_TAG, "isVideo = " + voiceSettings);
             dc.isVoicePrivacy =  (0 != p.readInt());
-            //Some Samsung magic data for Videocalls
-            voiceSettings = p.readInt();
-            //printing it to cosole for later investigation
-            Log.d(LOG_TAG, "Samsung magic = " + voiceSettings);
             dc.number = p.readString();
             Log.d(LOG_TAG, "number = " + dc.number);
             int np = p.readInt();

venky99/frameworks_base_disabled

telephony/java/com/android/internal/telephony/Smdk4210RIL.java
@@ -684,6 +684,7 @@ public class Smdk4210RIL extends RIL implements CommandsInterface {
     responseCallList(Parcel p) {
         int num;
         int voiceSettings;
+        int videoSettings;
         ArrayList<DriverCall> response;
         DriverCall dc;
         int dataAvail = p.dataAvail();
@@ -719,11 +720,10 @@ public class Smdk4210RIL extends RIL implements CommandsInterface {
             voiceSettings = p.readInt();
             dc.isVoice = (0 == voiceSettings) ? false : true;
             Log.d(LOG_TAG, "isVoice = " + dc.isVoice);
+            // For samsungs videocall feature
+            videoSettings = p.readInt();
+            Log.d(LOG_TAG, "isVideo = " + videoSettings);
             dc.isVoicePrivacy =  (0 != p.readInt());
-            //Some Samsung magic data for Videocalls
-            voiceSettings = p.readInt();
-            //printing it to cosole for later investigation
-            Log.d(LOG_TAG, "Samsung magic = " + voiceSettings);
             dc.number = p.readString();
             Log.d(LOG_TAG, "number = " + dc.number);
             int np = p.readInt();